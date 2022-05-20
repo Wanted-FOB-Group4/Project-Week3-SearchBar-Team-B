48c6404d1c7abc6dc436fc23e8fda16fb7df23db
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2022-05-20 11:31:55 +0900

    Feat : 퍼지 매칭 관련 함수 및 퍼지 매칭 컴포넌트 추가

---
+++ src/utils/fuzzyMathcingRegExp.ts
@@ -0,0 +1,8 @@
+import { koreanCharAt } from "./koreanCharAt";
+export const fuzzyMatchingRegExp = (value : string) => {
+  const fuzzyString = value
+    .split("")
+    .map((char) => `(${koreanCharAt(char)})`)
+    .join(".*?");
+  return fuzzyString;
+};
