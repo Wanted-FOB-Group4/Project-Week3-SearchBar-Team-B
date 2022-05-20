40d12c8752437d1b295297f082f666ed9c8cfb24
HyeongSeoku <gudtjr3437@naver.com>
2022-05-20 10:36:41 +0900

    Style : Add icons

--- src/assets/svgs/index.js
+++ src/assets/svgs/index.js
@@ -1,2 +1,4 @@
 export { ReactComponent as LogoImage } from './logo.svg'
 export { ReactComponent as CheckIcon } from './check.svg'
+export { ReactComponent as HeaderLogo } from './headerLogo.svg'
+export { ReactComponent as HumanIcon } from './human.svg'
