4d2c0302f1955332074ed9d9db529495f9a2da02
chichoon <chichoon.choi@gmail.com>
2022-05-20 12:36:43 +0900

    Feat: 안 쓰는 함수 삭제

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,5 @@
 import { getDiseaseData } from './getDiseaseData'
 
-import { getDiseaseDataJSON } from './getDiseaseDataJSON'
-
 import { getDiseaseDataFiltered } from './getDiseaseDataFiltered'
 
-export { getDiseaseData, getDiseaseDataJSON, getDiseaseDataFiltered }
+export { getDiseaseData, getDiseaseDataFiltered }
