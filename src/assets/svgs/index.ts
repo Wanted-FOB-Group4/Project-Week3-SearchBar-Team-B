7d2fa193ad27f89eed7377b663acf2023f014360
HyeongSeoku <gudtjr3437@naver.com>
2022-05-21 13:08:46 +0900

    Fix : Delete unused icon

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -1,5 +1,3 @@
 export { ReactComponent as SearchIcon } from './search.svg'
-export { ReactComponent as LogoImage } from './logo.svg'
-export { ReactComponent as CheckIcon } from './check.svg'
 export { ReactComponent as HeaderLogo } from './headerLogo.svg'
 export { ReactComponent as HumanIcon } from './human.svg'
