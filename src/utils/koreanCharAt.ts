81f7faae060d0203b15537cb587377c119c061d1
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2022-05-21 19:44:53 +0900

    Feat : fuzzymatching readme & Fix : fuzzymatching fnc

--- src/utils/koreanCharAt.ts
+++ src/utils/koreanCharAt.ts
@@ -1,9 +1,11 @@
 export const koreanCharAt = (target: string) => {
-  const offSet = '가'.charCodeAt(0)
+  const firstCode = '가'.charCodeAt(0)
+  const lastCode = '힣'.charCodeAt(0)
+  const targetCode = target.charCodeAt(0)
 
-  if (target.charCodeAt(0) >= offSet) {
-    if ((target.charCodeAt(0) - offSet) % 28 > 0) return target
-    const start = target.charCodeAt(0)
+  if (targetCode >= firstCode && targetCode <= lastCode) {
+    if ((targetCode - firstCode) % 28 > 0) return target
+    const start = targetCode
     const end = start + 27
     const regExpString = `[${target}\\u${start.toString(16)}-\\u${end.toString(16)}]`
     return regExpString
